39530abd0aea3953bc54eb0fe05ba3b61bfef11c
UpperCod <mtrujillo@outlook.cl>
2020-12-18 22:22:22 -0300

    add file:// to default resolution to node_modules

--- src/resolve.js
+++ src/resolve.js
@@ -4,16 +4,17 @@ import { readFile } from "fs/promises";
 import { exportMap } from "./export-map.js";
 import { cache, addDefaultExtension } from "./utils.js";
 
-const cwd = process.cwd();
-
-const defaultModuleFolder = join(cwd, "node_modules");
+const defaultModuleFolder = new URL(
+    "node_modules/",
+    "file://" + process.cwd().replace(/\\/g, "/") + "/"
+);
 
 export const packageName = /(@[^\/]+\/[^\/]+|[^\/]+)(?:\/){0,1}(.*)/;
 
 /**
  * Resolve the files of a package
  * @param {string} npm - name of the package to solve
- * @param {string} [moduleFolder] - allows to replace the module resolution path
+ * @param {string|URL} [moduleFolder] - allows to replace the module resolution path
  * @param {string} [pkgFileName] - allows replacing the name of the package.json, used for test.
  */
 export async function resolve(
@@ -23,7 +24,7 @@ export async function resolve(
 ) {
     const [, folder, subpathname] = npm.match(packageName);
 
-    const id = new URL(join(folder, pkgFileName), moduleFolder);
+    const id = new URL(folder + "/" + pkgFileName, moduleFolder);
 
     const pkg = await cache(id + "#pkg", async () =>
         JSON.parse(await readFile(id, "utf-8"))
