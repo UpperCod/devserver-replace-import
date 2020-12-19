bcec0b369cd5e4c908e108f71a8d0e9d5abfeb43
UpperCod <mtrujillo@outlook.cl>
2020-12-19 20:04:35 -0300

    fix export for commonjs

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,5 @@
 import pkg from "./package.json";
+import renameExtensions from "@betit/rollup-plugin-rename-extensions";
 
 export default {
     input: [
@@ -13,5 +14,13 @@ export default {
             format: "cjs",
             sourcemap: true,
         },
+    ],
+    plugins: [
+        renameExtensions({
+            include: ["**/*.js"],
+            mappings: {
+                ".js": ".cjs",
+            },
+        }),
     ],
 };
